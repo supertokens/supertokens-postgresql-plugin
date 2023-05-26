0c7a86577b16641fb522a53286c28973428533a7
Adithya Bhat <adithya@quiph.com>
2023-05-26 12:46:57 +0530

    feat: method to creat client

--- src/main/java/io/supertokens/storage/postgresql/queries/OAuth2Queries.java
+++ src/main/java/io/supertokens/storage/postgresql/queries/OAuth2Queries.java
@@ -16,15 +16,26 @@
 
 package io.supertokens.storage.postgresql.queries;
 
+import io.supertokens.pluginInterface.exceptions.StorageQueryException;
+import io.supertokens.pluginInterface.exceptions.StorageTransactionLogicException;
+import io.supertokens.pluginInterface.multitenancy.AppIdentifier;
+import io.supertokens.pluginInterface.multitenancy.TenantIdentifier;
 import io.supertokens.storage.postgresql.Start;
 import io.supertokens.storage.postgresql.config.Config;
 import io.supertokens.storage.postgresql.utils.Utils;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import static io.supertokens.storage.postgresql.QueryExecutorTemplate.update;
+import static io.supertokens.storage.postgresql.config.Config.getConfig;
+
 public class OAuth2Queries {
 
     static String getQueryToCreateOAuth2ClientTable(Start start) {
-        String schema = Config.getConfig(start).getTableSchema();
-        String oAuth2ClientTable = Config.getConfig(start).getOAuth2ClientTable();
+        String schema = getConfig(start).getTableSchema();
+        String oAuth2ClientTable = getConfig(start).getOAuth2ClientTable();
         // @formatter:off
         return "CREATE TABLE IF NOT EXISTS " + oAuth2ClientTable + " ("
                 + "app_id VARCHAR(64) DEFAULT 'public',"
@@ -37,20 +48,20 @@ public class OAuth2Queries {
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2ClientTable, null, "pkey")
                 + " PRIMARY KEY (app_id, client_id),"
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2ClientTable, "app_id", "fkey")
-                + " FOREIGN KEY(app_id) REFERENCES " + Config.getConfig(start).getAppsTable() + "(app_id) ON DELETE CASCADE);";
+                + " FOREIGN KEY(app_id) REFERENCES " + getConfig(start).getAppsTable() + "(app_id) ON DELETE CASCADE);";
         // @formatter:on
     }
 
     static String getQueryToCreateOAuth2ClientTableIndex(Start start) {
-        String oAuth2ClientTable = Config.getConfig(start).getOAuth2ClientTable();
+        String oAuth2ClientTable = getConfig(start).getOAuth2ClientTable();
         // psql does not automatically add index on foreign keys
         // hence adding index on foreign keys to support faster JOINS and DELETE ON CASCADE queries
         return "CREATE INDEX oauth2_client_app_id_index ON " + oAuth2ClientTable + "(app_id);";
     }
 
     static String getQueryToCreateOAuth2ScopesTable(Start start) {
-        String schema = Config.getConfig(start).getTableSchema();
-        String oAuth2ScopesTable = Config.getConfig(start).getOAuth2ScopesTable();
+        String schema = getConfig(start).getTableSchema();
+        String oAuth2ScopesTable = getConfig(start).getOAuth2ScopesTable();
         // @formatter:off
         return "CREATE TABLE IF NOT EXISTS " + oAuth2ScopesTable + " ("
                 + "app_id VARCHAR(64) DEFAULT 'public',"
@@ -58,21 +69,21 @@ public class OAuth2Queries {
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2ScopesTable, null, "pkey")
                 + " PRIMARY KEY (app_id, scope),"
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2ScopesTable, "app_id", "fkey")
-                + " FOREIGN KEY(app_id) REFERENCES " + Config.getConfig(start).getAppsTable() +
+                + " FOREIGN KEY(app_id) REFERENCES " + getConfig(start).getAppsTable() +
                 " (app_id) ON DELETE CASCADE);";
         // @formatter:on
     }
 
     static String getQueryToCreateOAuth2ScopesTableIndex(Start start) {
-        String oAuth2ScopesTable = Config.getConfig(start).getOAuth2ScopesTable();
+        String oAuth2ScopesTable = getConfig(start).getOAuth2ScopesTable();
         // psql does not automatically add index on foreign keys
         // hence adding index on foreign keys to support faster JOINS and DELETE ON CASCADE queries
         return "CREATE INDEX oauth2_scopes_app_id_index ON " + oAuth2ScopesTable + "(app_id);";
     }
 
     static String getQueryToCreateOAuth2ClientAllowedScopesTable(Start start) {
-        String schema = Config.getConfig(start).getTableSchema();
-        String oAuth2ClientAllowedScopesTable = Config.getConfig(start).getOAuth2ClientAllowedScopesTable();
+        String schema = getConfig(start).getTableSchema();
+        String oAuth2ClientAllowedScopesTable = getConfig(start).getOAuth2ClientAllowedScopesTable();
         // @formatter:off
         return "CREATE TABLE IF NOT EXISTS " + oAuth2ClientAllowedScopesTable + " ("
                 + "app_id VARCHAR(64) DEFAULT 'public',"
@@ -82,16 +93,16 @@ public class OAuth2Queries {
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2ClientAllowedScopesTable, null, "pkey")
                 + " PRIMARY KEY(app_id, client_id, scope),"
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2ClientAllowedScopesTable, "client_id", "fkey")
-                + " FOREIGN KEY(app_id,client_id) REFERENCES " + Config.getConfig(start).getOAuth2ClientTable()
+                + " FOREIGN KEY(app_id,client_id) REFERENCES " + getConfig(start).getOAuth2ClientTable()
                 + "(app_id, client_id) ON DELETE CASCADE,"
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2ClientAllowedScopesTable, "scope", "fkey")
-                + " FOREIGN KEY(app_id, scope) REFERENCES " + Config.getConfig(start).getOAuth2ScopesTable()
+                + " FOREIGN KEY(app_id, scope) REFERENCES " + getConfig(start).getOAuth2ScopesTable()
                 + "(app_id, scope) ON DELETE CASCADE);";
         // @formatter:on
     }
 
     static String getQueryToCreateOAuth2ClientAllowedScopesTableIndex(Start start) {
-        String oAuth2ClientAllowedScopesTable = Config.getConfig(start).getOAuth2ClientAllowedScopesTable();
+        String oAuth2ClientAllowedScopesTable = getConfig(start).getOAuth2ClientAllowedScopesTable();
         // psql does not automatically add index on foreign keys
         // hence adding index on foreign keys to support faster JOINS and DELETE ON CASCADE queries
         return "CREATE INDEX oauth2_client_allowed_scopes_client_id_index ON " + oAuth2ClientAllowedScopesTable + "(app_id, client_id);"
@@ -100,8 +111,8 @@ public class OAuth2Queries {
 
 
     static String getQueryToCreateOAuth2AuthcodeTable(Start start) {
-        String schema = Config.getConfig(start).getTableSchema();
-        String oAuth2AuthcodeTable = Config.getConfig(start).getOAuth2AuthcodeTable();
+        String schema = getConfig(start).getTableSchema();
+        String oAuth2AuthcodeTable = getConfig(start).getOAuth2AuthcodeTable();
         // @formatter:off
         return "CREATE TABLE IF NOT EXISTS " + oAuth2AuthcodeTable + " ("
                 + "app_id VARCHAR(64) DEFAULT 'public',"
@@ -128,24 +139,24 @@ public class OAuth2Queries {
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2AuthcodeTable, null, "pkey")
                 + " PRIMARY KEY (app_id, tenant_id, authorization_code_hash),"
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2AuthcodeTable, "client_id", "fkey")
-                + " FOREIGN KEY(app_id, client_id) REFERENCES " + Config.getConfig(start).getOAuth2ClientTable()
+                + " FOREIGN KEY(app_id, client_id) REFERENCES " + getConfig(start).getOAuth2ClientTable()
                 + "(app_id, client_id) ON DELETE CASCADE,"
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2AuthcodeTable, "session_handle", "fkey")
                 + " FOREIGN KEY(app_id, tenant_id, session_handle) REFERENCES "
-                + Config.getConfig(start).getSessionInfoTable() + "(app_id, tenant_id, session_handle) ON DELETE CASCADE );";
+                + getConfig(start).getSessionInfoTable() + "(app_id, tenant_id, session_handle) ON DELETE CASCADE );";
         // @formatter:on
     }
 
     static String getQueryToCreateOAuth2AuthcodeTableIndex(Start start) {
-        String oAuth2AuthcodeTable = Config.getConfig(start).getOAuth2AuthcodeTable();
+        String oAuth2AuthcodeTable = getConfig(start).getOAuth2AuthcodeTable();
         return "CREATE INDEX oauth2_authcode_client_id_index ON " + oAuth2AuthcodeTable + "(app_id, client_id);"
                 + "CREATE INDEX oauth2_authcode_session_handle_index ON " + oAuth2AuthcodeTable + "(app_id, tenant_id, session_handle);"
                 + "CREATE INDEX oauth2_authcode_expires_at_ms_index ON " + oAuth2AuthcodeTable + "(expires_at_ms);";
     }
 
     static String getQueryToCreateOAuth2TokenTable(Start start) {
-        String schema = Config.getConfig(start).getTableSchema();
-        String oAuth2TokenTable = Config.getConfig(start).getOAuth2TokenTable();
+        String schema = getConfig(start).getTableSchema();
+        String oAuth2TokenTable = getConfig(start).getOAuth2TokenTable();
         // @formatter:off
         return "CREATE TABLE IF NOT EXISTS " + oAuth2TokenTable + " ("
                 + "id CHAR(36) NOT NULL,"
@@ -167,19 +178,79 @@ public class OAuth2Queries {
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2TokenTable, null, "pkey")
                 + " PRIMARY KEY (app_id, tenant_id, id),"
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2TokenTable, "client_id", "fkey")
-                + " FOREIGN KEY(app_id, client_id) REFERENCES " + Config.getConfig(start).getOAuth2ClientTable()
+                + " FOREIGN KEY(app_id, client_id) REFERENCES " + getConfig(start).getOAuth2ClientTable()
                 + "(app_id, client_id) ON DELETE CASCADE,"
                 + "CONSTRAINT " + Utils.getConstraintName(schema, oAuth2TokenTable, "session_handle", "fkey")
-                + " FOREIGN KEY(app_id, tenant_id, session_handle) REFERENCES " + Config.getConfig(start).getSessionInfoTable()
+                + " FOREIGN KEY(app_id, tenant_id, session_handle) REFERENCES " + getConfig(start).getSessionInfoTable()
                 + "(app_id, tenant_id, session_handle) ON DELETE CASCADE);";
         // @formatter:on
     }
 
     static String getQueryToCreateOAuth2TokenTableIndex(Start start) {
-        String oAuth2TokenTable = Config.getConfig(start).getOAuth2TokenTable();
+        String oAuth2TokenTable = getConfig(start).getOAuth2TokenTable();
         return "CREATE INDEX oauth2_access_token_client_id_index ON " + oAuth2TokenTable + "(app_id, client_id);"
                 + "CREATE INDEX oauth2_access_token_session_handle_index ON " + oAuth2TokenTable + "(app_id,tenant_id,session_handle);"
                 + "CREATE INDEX oauth2_token_access_token_expires_at_ms_index ON " + oAuth2TokenTable + "(access_token_expires_at_ms);"
                 + "CREATE INDEX oauth2_token_refresh_token_expires_at_ms_index ON " + oAuth2TokenTable + "(refresh_token_expires_at_ms);";
+    }
+
+    public static void insertOAuth2Client_Transaction(Start start, Connection con,
+                                                      AppIdentifier appIdentifier, String client_id,
+                                                      String name, String clientSecretHash, List<String> redirectUris,
+                                                      long createdAt)
+            throws StorageQueryException, StorageTransactionLogicException
+    {
+
+        try {
+                String QUERY = "INSERT INTO " + getConfig(start).getOAuth2ClientTable()
+                        + "(app_id , client_id, name, client_secret_hash, redirect_uris, created_at, updated_at)" +
+                        " VALUES(?, ?, ?, ?, ?, ?, ?)";
+                update(con, QUERY, pst -> {
+                    pst.setString(1, appIdentifier.getAppId());
+                    pst.setString(2, client_id);
+                    pst.setString(3, name);
+                    pst.setString(4, clientSecretHash);
+                    pst.setString(5, String.join(",", redirectUris));
+                    pst.setLong(6, createdAt);
+                    pst.setLong(7, createdAt);
+                });
+
+            } catch (SQLException throwables) {
+                throw new StorageTransactionLogicException(throwables);
+            }
+    }
+
+    public static void insertOAuth2Scope( Start start, AppIdentifier appIdentifier, String scope)
+            throws SQLException, StorageQueryException {
+        String QUERY = "INSERT INTO " + getConfig(start).getOAuth2ScopesTable()
+                + "(app_id, scope)" + " VALUES(?, ?)";
+
+        update(start, QUERY, pst -> {
+            pst.setString(1, appIdentifier.getAppId());
+            pst.setString(2,scope);
+        });
+    }
+
+    public static void createOAuth2AuthorizationCode(Start start, TenantIdentifier tenantIdentifier, String codeHash, String sessionHandle, String clientId, long createdAtMs,
+                                                     long expiresAtMs, List<String> scope, String redirectUri, String accessType,
+                                                     String codeChallenge, String codeChallengeMethod)
+            throws SQLException, StorageQueryException {
+        String QUERY = "INSERT INTO " + getConfig(start).getOAuth2AuthcodeTable()
+                + "(app_id, tenant_id, authorization_code_hash, session_handle, client_id, created_at_ms, expires_at_ms, " + "scopes,access_type, code_challenge, code_challenge_method)" +
+                " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        update(start, QUERY, pst -> {
+            pst.setString(1, tenantIdentifier.getAppId());
+            pst.setString(2, tenantIdentifier.getTenantId());
+            pst.setString(3, codeHash);
+            pst.setString(4, sessionHandle);
+            pst.setString(5, clientId);
+            pst.setLong(6, createdAtMs);
+            pst.setLong(7, expiresAtMs);
+            pst.setString(8, String.join(",", scope));
+            pst.setString(9, accessType);
+            pst.setString(10,codeChallenge );
+            pst.setString(10,codeChallengeMethod );
+        });
+
     }
 }
