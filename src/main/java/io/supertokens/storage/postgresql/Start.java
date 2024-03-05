4bf90e0a037cf5312b8f6e3aed76258c6fbcf061
Sattvik Chakravarthy <sattvik@supertokens.com>
2024-03-05 13:09:25 +0530

    fix: fixes storage handling for non-auth recipes (#203)
    * fix: tests
    
    * fix: user role table constraint
    
    * fix: pr comments
    
    * fix: according to updated interface
    
    * fix: user roles
    
    * fix: version and changelog
    
    * fix: plugin interface version

--- src/main/java/io/supertokens/storage/postgresql/Start.java
+++ src/main/java/io/supertokens/storage/postgresql/Start.java
@@ -1930,7 +1930,7 @@ public class Start
 
     @Override
     public void addRoleToUser(TenantIdentifier tenantIdentifier, String userId, String role)
-            throws StorageQueryException, UnknownRoleException, DuplicateUserRoleMappingException,
+            throws StorageQueryException, DuplicateUserRoleMappingException,
             TenantOrAppNotFoundException {
         try {
             UserRolesQueries.addRoleToUser(this, tenantIdentifier, userId, role);
@@ -1938,9 +1938,6 @@ public class Start
             if (e instanceof PSQLException) {
                 PostgreSQLConfig config = Config.getConfig(this);
                 ServerErrorMessage serverErrorMessage = ((PSQLException) e).getServerErrorMessage();
-                if (isForeignKeyConstraintError(serverErrorMessage, config.getUserRolesTable(), "role")) {
-                    throw new UnknownRoleException();
-                }
                 if (isPrimaryKeyError(serverErrorMessage, config.getUserRolesTable())) {
                     throw new DuplicateUserRoleMappingException();
                 }
@@ -2007,6 +2004,16 @@ public class Start
     public boolean deleteRole(AppIdentifier appIdentifier, String role) throws StorageQueryException {
         try {
             return UserRolesQueries.deleteRole(this, appIdentifier, role);
+        } catch (SQLException e) {
+            throw new StorageQueryException(e);
+        }
+    }
+
+    @Override
+    public boolean deleteAllUserRoleAssociationsForRole(AppIdentifier appIdentifier, String role)
+            throws StorageQueryException {
+        try {
+            return UserRolesQueries.deleteAllUserRoleAssociationsForRole(this, appIdentifier, role);
         } catch (SQLException e) {
             throw new StorageQueryException(e);
         }
