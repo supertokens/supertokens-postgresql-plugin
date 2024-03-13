c91dea71c8d3f7cc84f336f0ba2aafc8ce1449c4
Sattvik Chakravarthy <sattvik@gmail.com>
2024-03-13 20:20:10 +0530

    fix: one million users

--- src/test/java/io/supertokens/storage/postgresql/test/OneMillionUsersTest.java
+++ src/test/java/io/supertokens/storage/postgresql/test/OneMillionUsersTest.java
@@ -385,9 +385,9 @@ public class OneMillionUsersTest {
 
     @Test
     public void testCreatingOneMillionUsers() throws Exception {
-//        if (System.getenv("ONE_MILLION_USERS_TEST") == null) {
-//            return;
-//        }
+        if (System.getenv("ONE_MILLION_USERS_TEST") == null) {
+            return;
+        }
 
         String[] args = {"../"};
         TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
@@ -443,13 +443,18 @@ public class OneMillionUsersTest {
         }
 
         sanityCheckAPIs(process.getProcess());
+        allUserIds.clear();
+        allPrimaryUserIds.clear();
+        userIdMappings.clear();
+        primaryUserIdMappings.clear();
+
+        process.kill(false);
 
         Runtime.getRuntime().gc();
         System.gc();
         System.runFinalization();
         Thread.sleep(10000);
-
-        process.kill(false);
+        
         process = TestingProcessManager.start(args, false);
         Utils.setValueInConfig("firebase_password_hashing_signer_key",
                 "gRhC3eDeQOdyEn4bMd9c6kxguWVmcIVq/SKa0JDPFeM6TcEevkaW56sIWfx88OHbJKnCXdWscZx0l2WbCJ1wbg==");
@@ -487,7 +492,7 @@ public class OneMillionUsersTest {
         memoryChecker.join();
 
         System.out.println("Max memory used: " + (maxMemory.get() / (1024 * 1024)) + " MB");
-        assert maxMemory.get() < 256 * 1024 * 1024; // must be less than 320 mb
+        assert maxMemory.get() < 256 * 1024 * 1024; // must be less than 256 mb
 
         process.kill();
         assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
