3fbacda8b0c12aad5e35e0e84647b904ece9234c
Sattvik <sattvik@Sattviks-MacBook-Pro.local>
2025-03-27 11:15:17 +0530

    fix: startDb.sh

--- startDb.sh
+++ startDb.sh
@@ -1,6 +1,6 @@
 docker run --rm --name postgres -e 'POSTGRES_USER=root' -e 'POSTGRES_PASSWORD=root' -d -p 5432:5432 -v ~/Desktop/db/pstgres:/var/lib/postgresql/data postgres -c 'max_connections=10000'
 
-sleep 5
+sleep 30
 
 docker exec postgres psql -U root root -c 'create database supertokens;'
 docker exec postgres psql -U root root -c 'create database st0;'
